0b3ffc1a61c8e355f59b945f729c036bb47c005a
Anoncomrade993 <ifeanyiotah001@gmail.com>
2024-03-17 23:30:24 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 mod Utils;
-use image::{GenericImageView, ImageBuffer, Rgb};
-
+use image::GenericImageView;
+use Utils::helpers::Block;
 
 fn main() {
+    let (w,h,vec) = read_image("./assets/specimen_1.jpg");
+    let mut cvec = vec.clone();
+    println!("{} * {}",w,h);
+    println!("{:?}",&cvec[..10]);
     
+    let d = 2u32;
+    let res Block::rotate(&mut cvec,d)
+    println!("{:?}",&res[..10]);
  }
 
 fn read_image(path: &str) -> (u32,u32,Vec<u8>){
