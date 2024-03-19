996efb6f95066c926226d7a4b0cc2ba1147c8d30
Anoncomrade993 <ifeanyiotah001@gmail.com>
2024-03-19 22:51:55 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -7,44 +7,47 @@ fn main() {
 
     let (w, h, vec) = read_image(path);
     let mut cvec = vec.clone();
-    println!("{} * {}", w, h);
-    println!("length :: {}", cvec.len());
-    println!("Pixels::\n{:?}", &cvec[0..10]);
-
-    let d: usize = 2;
-    caller(&mut cvec, d);
-    //println!("{:?}", res);
+    let outpath = "src/assets/output.png";
+    let res = caller(&mut cvec, d);
+   let width = cvec[0].len();
+   let height = cvec.len();
+  let _ = create_image(output,width, height, res);
+    
 }
 
 fn caller(vec: &mut Vec<Vec<u8>>, d: usize) -> () {
     let res = block::rotate(vec, d);
-    println!("Rotation::\n{:?}", &res[0..10]);
+    res
 }
 
 
 
 
-fn create_rgba_image(path:&str,width: u32, height: u32, data: Vec<Vec<u8>>) -> Result< ImageBuffer<Rgba<u8>, Vec<u8>>,image::error::Error> {
-    // Create an ImageBuffer with RGBA colors
-    let mut img = ImageBuffer::from_vec(width, height, vec![0; width * height * 4]);
+/*fn create_image(path:&str,width: u32, height: u32, data: Vec<Vec<u8>>) -> Result<(),image::ImageError> {
+    let vec = data.clone();
+    let 1dvec :Vec<u8> = vec.iter().flat_map(|d|{vec![d]}).collect();
+    let pic = image::RgbaImage::from_raw(width as usize, height as usize,1dvec);
+    
+    
+}*/
+fn create_image(path: &str, width: u32, height: u32, data: Vec<Vec<u8>>) -> Result<(), image::ImageError> {
+    let mut 1dvec: Vec<u8> = Vec::new();
 
-    // Iterate over each pixel and set its color
-    for (x, row) in data.iter().enumerate() {
-        for (y, pixel) in row.iter().enumerate() {
-            // Convert u8 values to f32 and scale to [0, 1]
-            let color = (*pixel as f32) / 255.0;
-            // Set RGBA pixel
-            img.put_pixel(x as u32, y as u32, Rgba([
-    data[y][x * 4] as f32 / 255.0,
-    data[y][x * 4 + 1] as f32 / 255.0,
-    data[y][x * 4 + 2] as f32 / 255.0,
-    1.0,  // Keep alpha at 1.0 (fully opaque)
-]));
- }
+    for row in data {
+        for pixel in row {
+            1dvec.push(pixel);
+        }
     }
-    img.save(path)?;
-}
 
+    let pic = image::RgbaImage::from_raw(width as u32, height as u32, 1dvec)
+        .ok_or(image::ImageError::Parameter(image::error::ParameterError::from_kind(
+            image::error::ParameterErrorKind::DimensionMismatch,
+        )))?;
+
+    pic.save(path)?;
+    println!("saved picture");
+    Ok(())
+}
 fn read_image(path: &str) -> (u32, u32, Vec<Vec<u8>>) {
     let im = image::open(path).unwrap();
     let (w, h) = im.dimensions();
